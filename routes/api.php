5c8c4fa208606340f1074bdf61e478e4f5a1386b
girihanbudi <Ghanbudi@gmail.com>
2025-12-23 15:52:10 +0700

    fix: remove frontend side
    Removed all frontend build files and Node toolchain (package.json, Vite config, resources/css/js, etc.) to focus on backend API development

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/ping', function () {
     return response()->json([
-        'message' => 'Backend is running.',
+        'message' => 'pong',
     ]);
 });
